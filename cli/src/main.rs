412b3c2d99d92f9ea62b82fdac29dc369f827c8e
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2020-12-28 13:39:55 +0100

    use structopt instead of clap, introducing snafu to cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,176 +1,205 @@
 extern crate ccfs_commons;
-extern crate clap;
 extern crate reqwest;
 
 use ccfs_commons::{Chunk, ChunkServer, File, CHUNK_SIZE};
-use clap::{App, Arg, SubCommand};
 use futures_util::StreamExt;
 use reqwest::multipart::Part;
+use snafu::{ResultExt, Snafu};
 use std::collections::HashMap;
-use std::error::Error;
+use std::fmt;
 use std::path::{Path, PathBuf};
 use std::str;
+use structopt::StructOpt;
 use tokio::fs::File as FileFS;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let matches = App::new("Chop-Chop File System")
-        .version("1.0")
-        .author("Zoran L. <lazarevic.zoki91@gmail.com>")
-        .about("A distrubuted highly available file system")
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .default_value("./cli_config.yaml")
-                .takes_value(true),
-        )
-        .subcommand(
-            SubCommand::with_name("upload")
-                .about("upload a file to the CCFS")
-                .arg(
-                    Arg::with_name("file_path")
-                        .help("absolute or relative path to the file")
-                        .index(1)
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("download")
-                .about("move a local file to the CCFS")
-                .arg(
-                    Arg::with_name("file_id")
-                        .help("the name of the file on the CCFS")
-                        .index(1)
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("file_path")
-                        .help("absolute or relative path where the file will be saved"),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("remove")
-                .about("remove a file from the CCFS")
-                .arg(
-                    Arg::with_name("file_path")
-                        .help("absolute or relative path to the file")
-                        .index(1)
-                        .required(true),
-                ),
-        )
-        .get_matches();
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("Unable to read file metadata {}: {}", path.display(), source))]
+    ReadMetadata {
+        source: tokio::io::Error,
+        path: PathBuf,
+    },
 
-    let config_file_path = matches.value_of("config").unwrap_or_default();
-    let path = Path::new(config_file_path);
+    #[snafu(display("Request to {} failed: {}", url, source))]
+    FailedRequest { source: reqwest::Error, url: String },
+
+    #[snafu(display("Unable to parse to json: {}", source))]
+    ParseJson { source: reqwest::Error },
+
+    #[snafu(display("Unable to {} file {}: {}", action, path.display(), source))]
+    FileIO {
+        source: tokio::io::Error,
+        path: PathBuf,
+        action: FileAction,
+    },
+}
+
+#[derive(Debug)]
+enum FileAction {
+    Read,
+    Write,
+    Create,
+    Open,
+}
+impl fmt::Display for FileAction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FileAction::Read => write!(f, "read"),
+            FileAction::Write => write!(f, "write"),
+            FileAction::Create => write!(f, "create"),
+            FileAction::Open => write!(f, "open"),
+        }
+    }
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, StructOpt)]
+/// Chop-Chop File System
+///
+/// A distrubuted highly available file system
+struct CliOpts {
+    /// Sets a custom config file
+    #[structopt(short, long, default_value = "./cli_config.yaml")]
+    config: String,
+
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Upload files to the CCFS
+    Upload {
+        /// The local absolute or relative path to the file to be uploaded to CCFS
+        file_path: String,
+    },
+    /// Download file from the CCFS
+    Download {
+        /// The path of the file on CCFS
+        file_path: String,
+    },
+    /// Remove a file from the CCFS
+    Remove {
+        /// The path of the file on CCFS
+        file_path: String,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts = CliOpts::from_args();
+    let path = Path::new(&opts.config);
     if !path.exists() {
-        println!("{} file doesn't exists", config_file_path)
+        panic!("{} file doesn't exists", &opts.config)
     }
     if path.is_dir() {
-        println!("{} is a directory", config_file_path)
+        panic!("{} is a directory", &opts.config)
     }
 
-    let config_file = std::fs::File::open(path).unwrap();
-    let config_map: HashMap<String, String> = serde_yaml::from_reader(config_file).unwrap();
-    let meta_server_url = config_map.get("metadata-server-url").unwrap();
+    let mut config_file = FileFS::open(path).await.context(FileIO {
+        path,
+        action: FileAction::Open,
+    })?;
+    let mut content = String::new();
+    FileFS::read_to_string(&mut config_file, &mut content)
+        .await
+        .context(FileIO {
+            path,
+            action: FileAction::Read,
+        })?;
+    let config_map: HashMap<String, String> =
+        serde_yaml::from_str(&content).expect("Couldn't deserialize config yaml");
+    let meta_server_url = config_map
+        .get("metadata-server-url")
+        .expect("Couldn't find metadata-server-url config");
     let client = reqwest::Client::new();
 
-    if let Some(ref matches) = matches.subcommand_matches("upload") {
-        upload(
-            &meta_server_url,
-            client,
-            matches.value_of("file_path").unwrap(),
-        )
-        .await
-        .unwrap();
-    } else if let Some(ref matches) = matches.subcommand_matches("download") {
-        let file_id = matches.value_of("file_id").unwrap();
-        let mut path_buf = PathBuf::new();
-        let path;
-        if let Some(path_str) = matches.value_of("file_path") {
-            path = Path::new(path_str);
-            if !path.exists() {
-                println!("path doesn't exist")
-            }
-            if !path.is_dir() {
-                println!("target {} is not a directory", path_str)
-            }
-        } else {
-            path_buf.push(".");
-            path_buf.push(file_id);
-            path = path_buf.as_path();
+    match opts.cmd {
+        Command::Upload { file_path } => {
+            upload(&meta_server_url, client, &file_path).await?;
         }
-        download(&meta_server_url, client, file_id, path).await?;
-    } else if let Some(ref _matches) = matches.subcommand_matches("remove") {
-        unimplemented!("Not implemented yet :(")
-    } else {
-        println!("Some other subcommand was used");
+        Command::Download { file_path } => {
+            download(&meta_server_url, client, Path::new(&file_path), None).await?;
+        }
+        Command::Remove { file_path: _path } => {
+            unimplemented!("Not implemented yet :(")
+        }
     }
     Ok(())
 }
 
-async fn upload(
+async fn upload<T: AsRef<Path>>(
     meta_server_url: &str,
     client: reqwest::Client,
-    path: &str,
-) -> Result<(), reqwest::Error> {
-    let file_path = Path::new(path);
-    if file_path.exists() {
-        let size = file_path.metadata().unwrap().len();
-        let file_data = File::new(file_path.to_str().unwrap().to_string(), size);
-        let file_resp: Result<File, _> = client
-            .post(&format!("{}/api/files/upload", meta_server_url))
+    file_path: T,
+) -> Result<()> {
+    let path = file_path.as_ref();
+    if path.exists() && !path.is_dir() {
+        let file_meta = path.metadata().context(ReadMetadata { path })?;
+        let file_data = File::new(
+            path.file_name().unwrap().to_str().unwrap().to_string(),
+            file_meta.len(),
+        );
+        let upload_url = format!("{}/api/files/upload", meta_server_url);
+        let file: File = client
+            .post(&upload_url)
             .json(&file_data)
             .send()
-            .await?
+            .await
+            .context(FailedRequest { url: upload_url })?
             .json()
-            .await;
+            .await
+            .context(ParseJson)?;
+        let servers_url = format!("{}/api/servers", meta_server_url);
         let servers: Vec<ChunkServer> = client
-            .get(&format!("{}/api/servers", meta_server_url))
+            .get(&servers_url)
             .send()
-            .await?
+            .await
+            .context(FailedRequest { url: servers_url })?
             .json()
-            .await?;
-        match file_resp {
-            Ok(file) => {
-                println!("file id: {}", file.id);
-                let mut i = 0usize;
-                let mut file_part = 1usize;
-                let mut f = FileFS::open(file_path).await.unwrap();
+            .await
+            .context(ParseJson)?;
+        let mut i = 0;
+        let mut file_part = 0usize;
+        let mut f = FileFS::open(path).await.context(FileIO {
+            path,
+            action: FileAction::Open,
+        })?;
 
-                loop {
-                    let mut chunk = Vec::with_capacity(CHUNK_SIZE as usize);
-                    let n = f.read_buf(&mut chunk).await.unwrap();
-                    if n == 0 && chunk.is_empty() {
-                        break;
-                    }
-                    let form = reqwest::multipart::Form::new()
-                        .text("file_id", file.id.to_string())
-                        .text("file_part_num", file_part.to_string())
-                        .part("file", Part::bytes(chunk));
-                    let server = &servers[i];
-                    i += 1;
-                    client
-                        .post(format!("{}/api/upload", server.address).as_str())
-                        .multipart(form)
-                        .send()
-                        .await?;
-
-                    file_part += 1;
-
-                    if n < CHUNK_SIZE as usize {
-                        break;
-                    }
-                }
-                println!("Completed file upload");
+        loop {
+            let mut chunk = Vec::with_capacity(CHUNK_SIZE as usize);
+            let n = f.read_buf(&mut chunk).await.context(FileIO {
+                path,
+                action: FileAction::Read,
+            })?;
+            if n == 0 && chunk.is_empty() {
+                break;
             }
-            _ => println!("Error while uploading file"),
+            let form = reqwest::multipart::Form::new()
+                .text("file_id", file.id.to_string())
+                .text("file_part_num", file_part.to_string())
+                .part("file", Part::bytes(chunk));
+            let server = &servers[i];
+            i += 1;
+            let upload_url = format!("{}/api/upload", server.address);
+            client
+                .post(&upload_url)
+                .multipart(form)
+                .send()
+                .await
+                .context(FailedRequest { url: upload_url })?;
+
+            file_part += 1;
+
+            if n < CHUNK_SIZE as usize {
+                break;
+            }
         }
+        println!("Completed file upload");
     } else {
-        println!("The file {} doesn't exists", path);
+        panic!("The file {} doesn't exists", path.display());
     }
     Ok(())
 }
@@ -178,59 +207,61 @@ async fn upload(
 async fn download(
     meta_server_url: &str,
     client: reqwest::Client,
-    file_id: &str,
-    path: &Path,
-) -> Result<(), reqwest::Error> {
+    file_path: &Path,
+    target_path: Option<&Path>,
+) -> Result<()> {
     // get chunks and merge them into a file
-    let file_resp: Result<File, _> = client
-        .get(
-            reqwest::Url::parse(format!("{}/api/files/{}", meta_server_url, &file_id).as_str())
-                .unwrap(),
-        )
+    let file_url = format!(
+        "{}/api/files/{}",
+        meta_server_url,
+        &file_path.file_name().unwrap().to_str().unwrap().to_string()
+    );
+    let file: File = client
+        .get(&file_url)
         .send()
-        .await?
+        .await
+        .context(FailedRequest { url: file_url })?
         .json()
-        .await;
-    if let Ok(file) = file_resp {
-        let chunks_resp: Result<Vec<Chunk>, _> = client
-            .get(
-                reqwest::Url::parse(
-                    format!("{}/api/chunks/file/{}", meta_server_url, &file.id).as_str(),
-                )
-                .unwrap(),
-            )
+        .await
+        .context(ParseJson)?;
+    let chunks_url = format!("{}/api/chunks/file/{}", meta_server_url, &file.id);
+    let mut chunks: Vec<Chunk> = client
+        .get(&chunks_url)
+        .send()
+        .await
+        .context(FailedRequest { url: chunks_url })?
+        .json()
+        .await
+        .context(ParseJson)?;
+    chunks.sort_by(|a, b| a.file_part_num.cmp(&b.file_part_num));
+    let default_path = format!("./{}", file.name);
+    let path = target_path.unwrap_or_else(|| Path::new(&default_path));
+    let mut file = FileFS::create(path).await.context(FileIO {
+        path,
+        action: FileAction::Create,
+    })?;
+    for chunk in chunks.iter() {
+        let chunk_url = format!("{}/api/servers/{}", meta_server_url, &chunk.server_id);
+        let server: ChunkServer = client
+            .get(&chunk_url)
             .send()
-            .await?
+            .await
+            .context(FailedRequest { url: chunk_url })?
             .json()
-            .await;
-        if let Ok(mut chunks) = chunks_resp {
-            chunks.sort_by(|a, b| a.file_part_num.cmp(&b.file_part_num));
-            let mut file = FileFS::create(path).await.unwrap();
-            for chunk in chunks.iter() {
-                let server: ChunkServer = client
-                    .get(
-                        reqwest::Url::parse(
-                            format!("{}/api/servers/{}", meta_server_url, &chunk.server_id)
-                                .as_str(),
-                        )
-                        .unwrap(),
-                    )
-                    .send()
-                    .await?
-                    .json()
-                    .await?;
-                let mut stream = reqwest::get(
-                    reqwest::Url::parse(
-                        format!("{}/api/download/{}", server.address, &chunk.id).as_str(),
-                    )
-                    .unwrap(),
-                )
-                .await?
-                .bytes_stream();
-                while let Some(content) = stream.next().await {
-                    file.write(&content.unwrap()).await.unwrap();
-                }
-            }
+            .await
+            .context(ParseJson)?;
+        let download_url = format!("{}/api/download/{}", server.address, &chunk.id);
+        let mut stream = client
+            .get(&download_url)
+            .send()
+            .await
+            .context(FailedRequest { url: download_url })?
+            .bytes_stream();
+        while let Some(content) = stream.next().await {
+            file.write(&content.unwrap()).await.context(FileIO {
+                path,
+                action: FileAction::Write,
+            })?;
         }
     }
     Ok(())
