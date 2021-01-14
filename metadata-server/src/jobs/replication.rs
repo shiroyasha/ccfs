b7083556b1cd0e271b0c769bb5d77caebbac7c3b
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-01-14 20:36:47 +0100

    split replicate_chunks fn

--- metadata-server/src/jobs/replication.rs
+++ metadata-server/src/jobs/replication.rs
@@ -2,16 +2,16 @@ use crate::errors::*;
 use crate::{ChunkServersMap, ChunksMap, FilesMap};
 use actix_web::client::Client;
 use ccfs_commons::result::CCFSResult;
-use ccfs_commons::FileStatus;
+use ccfs_commons::{Chunk, ChunkServer, File, FileStatus};
 use futures::future::{join_all, FutureExt, LocalBoxFuture};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use tokio::time::{delay_for, Duration};
+use uuid::Uuid;
 
 pub async fn start_replication_job(files: FilesMap, chunks: ChunksMap, servers: ChunkServersMap) {
     loop {
         delay_for(Duration::from_secs(20)).await;
-        if let Err(err) = replicate_chunks(files.clone(), chunks.clone(), servers.clone(), 3).await
-        {
+        if let Err(err) = replicate_files(files.clone(), chunks.clone(), servers.clone(), 3).await {
             // TODO: replace with logger
             println!("Error while creating replicas: {:?}", err);
         } else {
@@ -20,7 +20,7 @@ pub async fn start_replication_job(files: FilesMap, chunks: ChunksMap, servers: 
     }
 }
 
-fn replicate_chunks(
+fn replicate_files(
     files_map: FilesMap,
     chunks_map: ChunksMap,
     servers_map: ChunkServersMap,
@@ -40,52 +40,80 @@ fn replicate_chunks(
                 false => None,
             })
             .collect::<HashSet<_>>();
-        for f in active_files {
-            for chunk in f.chunks.iter() {
-                if let Some(replicas) = chunks.get(chunk) {
-                    let active_replicas = replicas
-                        .iter()
-                        .filter(|c| active_servers.contains(&c.server_id))
-                        .map(|c| &c.server_id)
-                        .collect::<HashSet<_>>();
-                    if active_replicas.is_empty() {
-                        continue;
-                    }
-                    let from_server = &servers
-                        .get(active_replicas.iter().next().unwrap())
-                        .unwrap()
-                        .address;
-                    if active_replicas.len() < required_replicas {
-                        let mut remaining = required_replicas - active_replicas.len();
-                        let server_candidates = &active_servers - &active_replicas;
-                        if !server_candidates.is_empty() {
-                            let mut iter = server_candidates.iter().peekable();
-                            while remaining > 0 && iter.peek().is_some() {
-                                let requests = (0..remaining).filter_map(|_| {
-                                    let s_id = iter.next()?;
-                                    let target_server = &servers.get(s_id)?.address;
-                                    Some(
-                                        c.post(format!("{}/api/replicate", &from_server))
-                                            .header("x-ccfs-chunk-id", chunk.to_string())
-                                            .header("x-ccfs-file-id", f.id.to_string())
-                                            .header("x-ccfs-server-url", target_server.clone())
-                                            .send(),
-                                    )
-                                });
-                                let responses = join_all(requests).await;
-                                let success_responses =
-                                    responses.into_iter().filter_map(|resp| match resp {
-                                        Ok(r) if r.status().is_success() => Some(r),
-                                        _ => None,
-                                    });
-                                remaining -= success_responses.count();
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        let futures = active_files
+            .map(|f| replicate_file(&c, f, &chunks, &active_servers, &servers, required_replicas));
+        join_all(futures).await;
         Ok(())
     }
     .boxed_local()
+}
+
+async fn replicate_file(
+    c: &Client,
+    file: &File,
+    chunks: &HashMap<Uuid, HashSet<Chunk>>,
+    active_servers: &HashSet<&Uuid>,
+    servers: &HashMap<Uuid, ChunkServer>,
+    required_replicas: usize,
+) -> CCFSResult<()> {
+    for chunk in file.chunks.iter() {
+        if let Some(replicas) = chunks.get(chunk) {
+            let replica_servers = replicas
+                .iter()
+                .filter(|c| active_servers.contains(&c.server_id))
+                .map(|c| &c.server_id)
+                .collect::<HashSet<_>>();
+            if !replica_servers.is_empty() && replica_servers.len() < required_replicas {
+                let target_server_candidates = active_servers - &replica_servers;
+                if !target_server_candidates.is_empty() {
+                    send_replication_requests(
+                        c,
+                        servers,
+                        &replica_servers,
+                        &target_server_candidates,
+                        &file.id,
+                        chunk,
+                        required_replicas,
+                    )
+                    .await?;
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn send_replication_requests(
+    c: &Client,
+    servers_map: &HashMap<Uuid, ChunkServer>,
+    replica_servers: &HashSet<&Uuid>,
+    target_servers: &HashSet<&Uuid>,
+    file_id: &Uuid,
+    chunk_id: &Uuid,
+    required_replicas: usize,
+) -> CCFSResult<()> {
+    let mut remaining = required_replicas - replica_servers.len();
+    let mut active_iter = replica_servers.iter().cycle();
+    let mut target_iter = target_servers.iter().peekable();
+    while remaining > 0 && target_iter.peek().is_some() {
+        let requests = (0..remaining).filter_map(|_| {
+            let s_id = target_iter.next()?;
+            let from_server = &servers_map.get(active_iter.next()?)?.address;
+            let target_server = &servers_map.get(s_id)?.address;
+            Some(
+                c.post(format!("{}/api/replicate", &from_server))
+                    .header("x-ccfs-chunk-id", chunk_id.to_string())
+                    .header("x-ccfs-file-id", file_id.to_string())
+                    .header("x-ccfs-server-url", target_server.clone())
+                    .send(),
+            )
+        });
+        let responses = join_all(requests).await;
+        let success_responses = responses.into_iter().filter_map(|resp| match resp {
+            Ok(r) if r.status().is_success() => Some(r),
+            _ => None,
+        });
+        remaining -= success_responses.count();
+    }
+    Ok(())
 }
