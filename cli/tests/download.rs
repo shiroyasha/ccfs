43538cec01c6008221d6d71f0141fbc04fd21bcd
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2021-01-21 20:19:47 +0100

    added tests for multipart/multiserver/dir_with_items cases

--- cli/tests/download.rs
+++ cli/tests/download.rs
@@ -1,22 +1,22 @@
 mod utils;
 
 use assert_cmd::prelude::*;
-use ccfs_commons::{Chunk, ChunkServer, FileMetadata};
+use ccfs_commons::{Chunk, ChunkServer, FileInfo, FileMetadata};
 use httpmock::{Method, MockServer};
 use predicates::prelude::*;
 use std::path::Path;
 use std::process::Command;
 use tempfile::tempdir_in;
-use tokio::fs::{read_to_string, File};
+use tokio::fs::read_to_string;
 use utils::{create_config_file, Cleanup};
 use uuid::Uuid;
 
 #[actix_rt::test]
-async fn test_download_dir() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_download_empty_dir() -> Result<(), Box<dyn std::error::Error>> {
     const TEST_DIR: &str = "test";
     let downloaded = Path::new(TEST_DIR);
     assert!(!downloaded.exists());
-    let _cleanup = Cleanup::new(vec![downloaded.to_path_buf()]);
+    let _cleanup = Cleanup::new(vec![downloaded.into()]);
     let dir_resp = FileMetadata::create_dir(TEST_DIR.into());
 
     let meta_server = MockServer::start();
@@ -48,17 +48,16 @@ async fn test_download_file() -> Result<(), Box<dyn std::error::Error>> {
     const TEST_FILE: &str = "test.txt";
     let downloaded = Path::new(TEST_FILE);
     assert!(!downloaded.exists());
-    let _cleanup = Cleanup::new(vec![downloaded.to_path_buf()]);
+    let _cleanup = Cleanup::new(vec![downloaded.into()]);
     let chunk_id = Uuid::new_v4();
     let server_id = Uuid::new_v4();
     let file_resp = FileMetadata::create_file(TEST_FILE.into(), 10, vec![chunk_id]);
     let file_id = match &file_resp.file_info {
-        ccfs_commons::FileInfo::File { id, .. } => *id,
+        FileInfo::File { id, .. } => *id,
         _ => unreachable!(),
     };
     let chunk = Chunk::new(chunk_id, file_id, server_id);
     let temp_dir = tempdir_in("./")?;
-    File::create(temp_dir.path().join(chunk.chunk_name())).await?;
 
     let chunk_server = MockServer::start();
     let chunk_server_val = ChunkServer::new(server_id, chunk_server.base_url());
@@ -72,7 +71,7 @@ async fn test_download_file() -> Result<(), Box<dyn std::error::Error>> {
     meta_server.mock(|when, then| {
         when.method(Method::GET)
             .path("/api/files")
-            .query_param("path", "./test.txt");
+            .query_param("path", TEST_FILE);
         then.status(200)
             .header("content-type", "application/json")
             .json_body_obj(&file_resp);
@@ -97,7 +96,7 @@ async fn test_download_file() -> Result<(), Box<dyn std::error::Error>> {
         .arg("-c")
         .arg(&config_file_path)
         .arg("download")
-        .arg("./test.txt")
+        .arg(TEST_FILE)
         .assert()
         .success()
         .stdout(predicate::str::contains("Finished downloading `test.txt`"));
@@ -110,6 +109,9 @@ async fn test_download_file() -> Result<(), Box<dyn std::error::Error>> {
 
 #[actix_rt::test]
 async fn test_download_not_existing() -> Result<(), Box<dyn std::error::Error>> {
+    const TEST_FILE: &str = "non-existing.txt";
+    let downloaded = Path::new(TEST_FILE);
+    assert!(!downloaded.exists());
     let temp_dir = tempdir_in("./")?;
 
     let meta_server = MockServer::start();
@@ -123,37 +125,34 @@ async fn test_download_not_existing() -> Result<(), Box<dyn std::error::Error>> 
         .arg("-c")
         .arg(&config_file_path)
         .arg("download")
-        .arg("./test.txt")
+        .arg(TEST_FILE)
         .assert()
         .failure()
         .stderr(predicate::str::contains(
             "Error: Request failed: File doesn't exist",
         ));
+    assert!(!downloaded.exists());
     Ok(())
 }
 
 #[actix_rt::test]
 async fn test_download_file_no_chunks_for_file() -> Result<(), Box<dyn std::error::Error>> {
-    const TEST_FILE: &str = "test.txt";
+    const TEST_FILE: &str = "test1.txt";
     let downloaded = Path::new(TEST_FILE);
     assert!(!downloaded.exists());
-    let _cleanup = Cleanup::new(vec![downloaded.to_path_buf()]);
     let chunk_id = Uuid::new_v4();
-    let server_id = Uuid::new_v4();
     let file_resp = FileMetadata::create_file(TEST_FILE.into(), 10, vec![chunk_id]);
     let file_id = match &file_resp.file_info {
-        ccfs_commons::FileInfo::File { id, .. } => *id,
+        FileInfo::File { id, .. } => *id,
         _ => unreachable!(),
     };
-    let chunk = Chunk::new(chunk_id, file_id, server_id);
     let temp_dir = tempdir_in("./")?;
-    File::create(temp_dir.path().join(chunk.chunk_name())).await?;
 
     let meta_server = MockServer::start();
     meta_server.mock(|when, then| {
         when.method(Method::GET)
             .path("/api/files")
-            .query_param("path", "./test.txt");
+            .query_param("path", TEST_FILE);
         then.status(200)
             .header("content-type", "application/json")
             .json_body_obj(&file_resp);
@@ -172,7 +171,7 @@ async fn test_download_file_no_chunks_for_file() -> Result<(), Box<dyn std::erro
         .arg("-c")
         .arg(&config_file_path)
         .arg("download")
-        .arg("./test.txt")
+        .arg(TEST_FILE)
         .assert()
         .failure()
         .stderr(predicate::str::contains(
@@ -183,20 +182,18 @@ async fn test_download_file_no_chunks_for_file() -> Result<(), Box<dyn std::erro
 
 #[actix_rt::test]
 async fn test_download_file_chunk_download_failure() -> Result<(), Box<dyn std::error::Error>> {
-    const TEST_FILE: &str = "test.txt";
+    const TEST_FILE: &str = "test2.txt";
     let downloaded = Path::new(TEST_FILE);
     assert!(!downloaded.exists());
-    let _cleanup = Cleanup::new(vec![downloaded.to_path_buf()]);
     let chunk_id = Uuid::new_v4();
     let server_id = Uuid::new_v4();
     let file_resp = FileMetadata::create_file(TEST_FILE.into(), 10, vec![chunk_id]);
     let file_id = match &file_resp.file_info {
-        ccfs_commons::FileInfo::File { id, .. } => *id,
+        FileInfo::File { id, .. } => *id,
         _ => unreachable!(),
     };
     let chunk = Chunk::new(chunk_id, file_id, server_id);
     let temp_dir = tempdir_in("./")?;
-    File::create(temp_dir.path().join(chunk.chunk_name())).await?;
 
     let chunk_server = MockServer::start();
     let chunk_server_val = ChunkServer::new(server_id, chunk_server.base_url());
@@ -210,7 +207,7 @@ async fn test_download_file_chunk_download_failure() -> Result<(), Box<dyn std::
     meta_server.mock(|when, then| {
         when.method(Method::GET)
             .path("/api/files")
-            .query_param("path", "./test.txt");
+            .query_param("path", TEST_FILE);
         then.status(200)
             .header("content-type", "application/json")
             .json_body_obj(&file_resp);
@@ -235,7 +232,7 @@ async fn test_download_file_chunk_download_failure() -> Result<(), Box<dyn std::
         .arg("-c")
         .arg(&config_file_path)
         .arg("download")
-        .arg("./test.txt")
+        .arg(TEST_FILE)
         .assert()
         .failure()
         .stderr(predicate::str::contains(
@@ -246,26 +243,24 @@ async fn test_download_file_chunk_download_failure() -> Result<(), Box<dyn std::
 
 #[actix_rt::test]
 async fn test_download_file_get_server_failure() -> Result<(), Box<dyn std::error::Error>> {
-    const TEST_FILE: &str = "test.txt";
+    const TEST_FILE: &str = "test3.txt";
     let downloaded = Path::new(TEST_FILE);
     assert!(!downloaded.exists());
-    let _cleanup = Cleanup::new(vec![downloaded.to_path_buf()]);
     let chunk_id = Uuid::new_v4();
     let server_id = Uuid::new_v4();
     let file_resp = FileMetadata::create_file(TEST_FILE.into(), 10, vec![chunk_id]);
     let file_id = match &file_resp.file_info {
-        ccfs_commons::FileInfo::File { id, .. } => *id,
+        FileInfo::File { id, .. } => *id,
         _ => unreachable!(),
     };
     let chunk = Chunk::new(chunk_id, file_id, server_id);
     let temp_dir = tempdir_in("./")?;
-    File::create(temp_dir.path().join(chunk.chunk_name())).await?;
 
     let meta_server = MockServer::start();
     meta_server.mock(|when, then| {
         when.method(Method::GET)
             .path("/api/files")
-            .query_param("path", "./test.txt");
+            .query_param("path", TEST_FILE);
         then.status(200)
             .header("content-type", "application/json")
             .json_body_obj(&file_resp);
@@ -288,11 +283,251 @@ async fn test_download_file_get_server_failure() -> Result<(), Box<dyn std::erro
         .arg("-c")
         .arg(&config_file_path)
         .arg("download")
-        .arg("./test.txt")
+        .arg(TEST_FILE)
         .assert()
         .failure()
         .stderr(predicate::str::contains(
             "Error: Failed to download some chunks",
         ));
+    assert!(!downloaded.exists());
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_download_dir_with_items() -> Result<(), Box<dyn std::error::Error>> {
+    const TEST_DIR: &str = "test_with_items";
+    const TEST_FILE: &str = "test4.txt";
+    let downloaded = Path::new(TEST_DIR);
+    let downloaded_file = downloaded.join(TEST_FILE);
+    assert!(!downloaded.exists());
+    let _cleanup = Cleanup::new(vec![downloaded.into()]);
+    let mut dir_resp = FileMetadata::create_dir(TEST_DIR.into());
+
+    let chunk_id = Uuid::new_v4();
+    let server_id = Uuid::new_v4();
+    let file_node = FileMetadata::create_file(TEST_FILE.into(), 10, vec![chunk_id]);
+    let file_id = match &file_node.file_info {
+        FileInfo::File { id, .. } => *id,
+        _ => unreachable!(),
+    };
+    match dir_resp.file_info {
+        FileInfo::Directory { ref mut children } => {
+            children.insert(file_node.name.clone(), file_node);
+        }
+        _ => unreachable!(),
+    }
+    let chunk = Chunk::new(chunk_id, file_id, server_id);
+    let temp_dir = tempdir_in("./")?;
+
+    let chunk_server = MockServer::start();
+    let chunk_server_val = ChunkServer::new(server_id, chunk_server.base_url());
+
+    chunk_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/download/{}", chunk.chunk_name()));
+        then.status(200).body("Test file content");
+    });
+    let meta_server = MockServer::start();
+    meta_server.mock(|when, then| {
+        when.method(Method::GET).path("/api/files");
+        then.status(200)
+            .header("content-type", "application/json")
+            .json_body_obj(&dir_resp);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/chunks/file/{}", file_id));
+        then.status(200)
+            .header("content-type", "application/json")
+            .json_body_obj(&vec![vec![chunk]]);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/servers/{}", server_id));
+        then.status(200)
+            .header("Content-Type", "application/json")
+            .json_body_obj(&chunk_server_val);
+    });
+
+    let config_file_path = create_config_file(&meta_server.base_url(), temp_dir.path()).await?;
+    Command::cargo_bin("cli")?
+        .arg("-c")
+        .arg(&config_file_path)
+        .arg("download")
+        .arg(TEST_DIR)
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "Finished downloading `test_with_items`",
+        ));
+
+    assert!(downloaded.exists());
+    assert!(downloaded.is_dir());
+    assert!(downloaded_file.exists());
+    assert!(downloaded_file.is_file());
+    assert_eq!(read_to_string(downloaded_file).await?, "Test file content");
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_download_file_with_multiple_parts() -> Result<(), Box<dyn std::error::Error>> {
+    const TEST_FILE: &str = "test-multipart.txt";
+    let downloaded = Path::new(TEST_FILE);
+    assert!(!downloaded.exists());
+    let _cleanup = Cleanup::new(vec![downloaded.into()]);
+    let chunk1_id = Uuid::new_v4();
+    let chunk2_id = Uuid::new_v4();
+    let server1_id = Uuid::new_v4();
+    let server2_id = Uuid::new_v4();
+    let file_resp = FileMetadata::create_file(TEST_FILE.into(), 10, vec![chunk1_id, chunk2_id]);
+    let file_id = match &file_resp.file_info {
+        FileInfo::File { id, .. } => *id,
+        _ => unreachable!(),
+    };
+    let chunk1 = Chunk::new(chunk1_id, file_id, server1_id);
+    let chunk2 = Chunk::new(chunk2_id, file_id, server2_id);
+    let temp_dir = tempdir_in("./")?;
+
+    let chunk_server1 = MockServer::start();
+    let chunk_server1_val = ChunkServer::new(server1_id, chunk_server1.base_url());
+    let chunk_server2 = MockServer::start();
+    let chunk_server2_val = ChunkServer::new(server2_id, chunk_server2.base_url());
+
+    chunk_server1.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/download/{}", chunk1.chunk_name()));
+        then.status(200).body("Test file content part1");
+    });
+    chunk_server2.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/download/{}", chunk2.chunk_name()));
+        then.status(200).body("Test file content part2");
+    });
+    let meta_server = MockServer::start();
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path("/api/files")
+            .query_param("path", TEST_FILE);
+        then.status(200)
+            .header("content-type", "application/json")
+            .json_body_obj(&file_resp);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/chunks/file/{}", file_id));
+        then.status(200)
+            .header("content-type", "application/json")
+            .json_body_obj(&vec![vec![chunk1], vec![chunk2]]);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/servers/{}", server1_id));
+        then.status(200)
+            .header("Content-Type", "application/json")
+            .json_body_obj(&chunk_server1_val);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/servers/{}", server2_id));
+        then.status(200)
+            .header("Content-Type", "application/json")
+            .json_body_obj(&chunk_server2_val);
+    });
+
+    let config_file_path = create_config_file(&meta_server.base_url(), temp_dir.path()).await?;
+    Command::cargo_bin("cli")?
+        .arg("-c")
+        .arg(&config_file_path)
+        .arg("download")
+        .arg(TEST_FILE)
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "Finished downloading `test-multipart.txt`",
+        ));
+
+    assert!(downloaded.exists());
+    assert!(downloaded.is_file());
+    assert_eq!(
+        read_to_string(downloaded).await?,
+        "Test file content part1Test file content part2"
+    );
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn test_download_file_with_multiple_parts_one_chunk_failure(
+) -> Result<(), Box<dyn std::error::Error>> {
+    const TEST_FILE: &str = "test-multipart-failed.txt";
+    let downloaded = Path::new(TEST_FILE);
+    assert!(!downloaded.exists());
+    let chunk1_id = Uuid::new_v4();
+    let chunk2_id = Uuid::new_v4();
+    let server1_id = Uuid::new_v4();
+    let server2_id = Uuid::new_v4();
+    let file_resp = FileMetadata::create_file(TEST_FILE.into(), 10, vec![chunk1_id, chunk2_id]);
+    let file_id = match &file_resp.file_info {
+        FileInfo::File { id, .. } => *id,
+        _ => unreachable!(),
+    };
+    let chunk1 = Chunk::new(chunk1_id, file_id, server1_id);
+    let chunk2 = Chunk::new(chunk2_id, file_id, server2_id);
+    let temp_dir = tempdir_in("./")?;
+
+    let chunk_server1 = MockServer::start();
+    let chunk_server1_val = ChunkServer::new(server1_id, chunk_server1.base_url());
+    let chunk_server2 = MockServer::start();
+
+    chunk_server1.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/download/{}", chunk1.chunk_name()));
+        then.status(200).body("Test file content part1");
+    });
+    chunk_server2.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/download/{}", chunk2.chunk_name()));
+        then.status(200).body("Test file content part2");
+    });
+    let meta_server = MockServer::start();
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path("/api/files")
+            .query_param("path", TEST_FILE);
+        then.status(200)
+            .header("content-type", "application/json")
+            .json_body_obj(&file_resp);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/chunks/file/{}", file_id));
+        then.status(200)
+            .header("content-type", "application/json")
+            .json_body_obj(&vec![vec![chunk1], vec![chunk2]]);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/servers/{}", server1_id));
+        then.status(200)
+            .header("Content-Type", "application/json")
+            .json_body_obj(&chunk_server1_val);
+    });
+    meta_server.mock(|when, then| {
+        when.method(Method::GET)
+            .path(format!("/api/servers/{}", server2_id));
+        then.status(500);
+    });
+
+    let config_file_path = create_config_file(&meta_server.base_url(), temp_dir.path()).await?;
+    Command::cargo_bin("cli")?
+        .arg("-c")
+        .arg(&config_file_path)
+        .arg("download")
+        .arg(TEST_FILE)
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Error: Failed to download some chunks",
+        ));
+    assert!(!downloaded.exists());
     Ok(())
 }
